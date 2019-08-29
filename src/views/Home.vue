3dcc99e9589477ca356a1a50bc236314e981c6ca
Brandon Satrom <bsatrom@gmail.com>
2019-08-29 10:49:02 -0500

    reverted back to previous commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <Main />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import Main from "../components/Main";
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  name: 'home',
   components: {
-    Main
+    HelloWorld
   }
-};
+}
 </script>
